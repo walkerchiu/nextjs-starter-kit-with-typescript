aa3759f08e83678a2983baa2888f572bffde6174
walkerchiu <chenjen.chiou@gmail.com>
2022-04-26 20:14:52 +0800

    Feat(Vendor): Add next-seo

---
+++ src/layouts/ExampleLayout.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+interface ComponentProps {
+  children: any,
+}
+
+const ExampleLayout: FC<ComponentProps> = ({
+  children,
+}) => {
+  return (
+    <main
+      className='w-full'
+    >
+      { children }
+    </main>
+  )
+}
+
+export default ExampleLayout
---
+++ src/modules/examples/Header.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import Link from "next/link";
+
+interface ComponentProps {
+  className?: string,
+  children?: any,
+}
+
+const Header: FC<ComponentProps> = ({
+  children,
+}) => {
+  return (
+    <section
+      role="navigation"
+      aria-label="Main"
+      className="w-full my-5 px-5 xl:px-10 py-5 flex flex-col xl:flex-row items-start xl:items-center justify-between shadow rounded"
+    >
+      <div
+        className="mb-4 lg:w-1/2"
+      >
+        <p
+          className="focus:outline-none text-2xl font-bold"
+        >
+          Example
+        </p>
+        <p
+          className="focus:outline-none font-normal text-gray-600 mt-1"
+        >
+          Description
+        </p>
+      </div>
+      <div
+        role="list"
+        className="flex items-center"
+      >
+        <Link
+          href="/"
+        >
+          <a
+            className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 focus:outline-none cursor-pointer flex justify-center items-center mx-2 py-2 px-4 rounded hover:bg-yellow-100 text-gray-500"
+          >
+            Home
+          </a>
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+export default Header
