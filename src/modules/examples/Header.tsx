04d06bf8d69ec6203ac2ed8ee1e08808438f533d
walkerchiu <chenjen.chiou@gmail.com>
2022-05-14 08:21:39 +0800

    Feat(Views): Update header of examples

--- src/modules/examples/Header.tsx
+++ src/modules/examples/Header.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 interface ComponentProps {
   className?: string,
-  children?: any,
+  title: string,
+  description: string,
 }
 
 const Header: FC<ComponentProps> = ({
-  children,
+  title,
+  description,
 }) => {
   return (
     <section
@@ -21,12 +23,12 @@ const Header: FC<ComponentProps> = ({
         <p
           className="focus:outline-none text-2xl font-bold"
         >
-          Example
+          Example: { title }
         </p>
         <p
           className="focus:outline-none font-normal text-gray-600 mt-1"
         >
-          Description
+          { description }
         </p>
       </div>
       <div
