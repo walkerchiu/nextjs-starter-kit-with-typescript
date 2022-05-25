082f400130a5d2f7be9daa0437e27c1cf11f91c8
walkerchiu <chenjen.chiou@gmail.com>
2022-05-25 21:13:10 +0800

    Feat(Module): Add basic example of useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -230,6 +230,17 @@ const Home: NextPage = () => {
                   </a>
                 </Link>
               </li>
+              <li>
+                <Link
+                  href="/examples/useEffect"
+                >
+                  <a
+                    className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+                  >
+                    useEffect
+                  </a>
+                </Link>
+              </li>
             </ol>
           </article>
           <article>
