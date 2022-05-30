fe1202c7efa53266bfddb043315a1beaad8b2e81
walkerchiu <chenjen.chiou@gmail.com>
2022-05-30 21:39:56 +0800

    Feat(Module): Add class component example showing how to handle the cleanup phase of useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -263,6 +263,17 @@ const Home: NextPage = () => {
                   </a>
                 </Link>
               </li>
+              <li>
+                <Link
+                  href="/examples/useEffect-class-component2"
+                >
+                  <a
+                    className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+                  >
+                    useEffect (Class Component 2)
+                  </a>
+                </Link>
+              </li>
             </ol>
           </article>
           <article>
