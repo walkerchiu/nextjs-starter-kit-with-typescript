cd4239c319d459589d16019c1d9f3f73a1f613a5
walkerchiu <chenjen.chiou@gmail.com>
2022-12-16 22:58:17 +0800

    Feat(Theme): Update footer of examples

--- src/modules/examples/Footer.tsx
+++ src/modules/examples/Footer.tsx
@@ -10,10 +10,12 @@ const Footer: FC<ComponentProps> = ({ children }) => {
     <section
       role="navigation"
       aria-label="Main"
-      className="border-t-2 inset-x-0 p-5 xl:p-10"
+      className="border-t-2 inset-x-0 p-5 md:p-10"
     >
       <div className="mb-4 sm:mb-0 md:mb-0 lg:mb-0 xl:mb-0">
-        <p className="focus:outline-none text-2xl font-bold">Reference</p>
+        <p className="focus:outline-none text-md md:text-xl font-bold">
+          Reference
+        </p>
         <div className="focus:outline-none font-normal mt-1 text-gray-600 dark:text-white mt-1">
           {children}
         </div>
