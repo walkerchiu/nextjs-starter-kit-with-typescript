61fa27d5bd03ab98155e03e262f0af81f8fb770c
walkerchiu <chenjen.chiou@gmail.com>
2023-04-16 11:26:20 +0800

    Feat(Config): Add Domain Routing for Locale Strategies

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
   i18n: {
     locales: ["en-US", "zh-TW"],
     defaultLocale: "en-US",
+
+    domains: [
+      {
+        // Note: subdomains must be included in the domain value to be matched
+        // e.g. www.example.com should be used if that is the expected hostname
+        domain: "example.com",
+        defaultLocale: "en-US",
+      },
+      {
+        domain: "example.tw",
+        defaultLocale: "zh-TW",
+        // specify other locales that should be redirected
+        // to this domain
+        locales: ["zh-TW"],
+      },
+    ],
   },
   images: {
     domains: ["via.placeholder.com"],
