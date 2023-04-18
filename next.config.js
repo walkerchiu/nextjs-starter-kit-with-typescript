fbd3926f86a86f0a3bf2bf2a08c20beb9c9babd4
walkerchiu <chenjen.chiou@gmail.com>
2023-04-18 20:45:54 +0800

    Docs(Config): Add localeDetection's default setting information

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ["en-US", "zh-TW"],
+    // When localeDetection is set to false Next.js will no longer automatically redirect based on the user's preferred locale
+    // and will only provide locale information detected from either the locale based domain or locale path as described above.
+    // Default: true
+    localeDetection: true,
     defaultLocale: "en-US",
 
     domains: [
