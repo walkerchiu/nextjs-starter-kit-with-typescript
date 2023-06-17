f22ff72bdf9b065ea971224c1f6d4d520971104d
walkerchiu <chenjen.chiou@gmail.com>
2023-06-17 19:52:04 +0800

    Feat(Module): Add grouped example of react-select

--- src/pages/react-select/grouped.tsx
+++ src/pages/react-select/grouped.tsx
@@ -1,41 +1,67 @@
+import { CSSProperties } from "react";
+
 import type { NextPage } from "next";
-import Link from "next/link";
+import Select from "react-select";
 
 import Footer from "../../layouts/Footer";
 import Header from "../../layouts/Header";
+import {
+  ColourOption,
+  colourOptions,
+  FlavourOption,
+  GroupedOption,
+  groupedOptions,
+} from "../../modules/react-select/data";
 
-const IndexPage: NextPage = () => {
+const groupStyles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+};
+const groupBadgeStyles: CSSProperties = {
+  backgroundColor: "#EBECF0",
+  borderRadius: "2em",
+  color: "#172B4D",
+  display: "inline-block",
+  fontSize: 12,
+  fontWeight: "normal",
+  lineHeight: "1",
+  minWidth: 1,
+  padding: "0.125em 0.5em",
+  textAlign: "center",
+};
+
+const formatGroupLabel = (data: GroupedOption) => (
+  <div style={groupStyles}>
+    <span>{data.label}</span>
+    <span style={groupBadgeStyles}>{data.options.length}</span>
+  </div>
+);
+
+const ReactSelectPage: NextPage = () => {
   return (
     <div className="flex flex-col h-screen">
       <Header
-        title="react-select"
+        title="react-select (Grouped)"
         description="A flexible and beautiful Select Input control for ReactJS with multiselect, autocomplete, async and creatable support."
       />
-      <main id="main" className="flex w-full flex-1 flex-col pt-5 pb-10 px-16">
-        <article>
-          <ol
-            style={{
-              listStyleType: "number",
-            }}
-          >
-            <li>
-              <Link
-                href="/react-select/basic"
-                className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
-              >
-                react-select (Basic)
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/react-select/grouped"
-                className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
-              >
-                react-select (Grouped)
-              </Link>
-            </li>
-          </ol>
-        </article>
+      <main
+        id="main"
+        className="w-full flex-1 flex-col items-center justify-center p-5"
+      >
+        <section
+          style={{
+            marginBottom: "20px",
+          }}
+        >
+          <div>
+            <Select<ColourOption | FlavourOption, false, GroupedOption>
+              defaultValue={colourOptions[1]}
+              options={groupedOptions}
+              formatGroupLabel={formatGroupLabel}
+            />
+          </div>
+        </section>
       </main>
       <Footer>
         <ol
@@ -74,4 +100,4 @@ const IndexPage: NextPage = () => {
   );
 };
 
-export default IndexPage;
+export default ReactSelectPage;
