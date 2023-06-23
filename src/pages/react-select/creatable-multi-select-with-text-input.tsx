e09a2ab6770c3b49a59535bda90f77389ff9f11c
walkerchiu <chenjen.chiou@gmail.com>
2023-06-23 20:50:11 +0800

    Feat(Module): Add creatable-multi-select-with-text-input example of react-select

---
+++ src/pages/react-select/creatable-multi-select-with-text-input.tsx
@@ -0,0 +1,106 @@
+import React, { KeyboardEventHandler } from "react";
+
+import type { NextPage } from "next";
+import CreatableSelect from "react-select/creatable";
+
+import Footer from "../../layouts/Footer";
+import Header from "../../layouts/Header";
+
+const components = {
+  DropdownIndicator: null,
+};
+
+interface Option {
+  readonly label: string;
+  readonly value: string;
+}
+
+const createOption = (label: string) => ({
+  label,
+  value: label,
+});
+
+const ReactSelectPage: NextPage = () => {
+  const [inputValue, setInputValue] = React.useState("");
+  const [value, setValue] = React.useState<readonly Option[]>([]);
+
+  const handleKeyDown: KeyboardEventHandler = (event) => {
+    if (!inputValue) return;
+    switch (event.key) {
+      case "Enter":
+      case "Tab":
+        setValue((prev) => [...prev, createOption(inputValue)]);
+        setInputValue("");
+        event.preventDefault();
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header
+        title="react-select (Creatable: Multi Select with Text Input)"
+        description="A flexible and beautiful Select Input control for ReactJS with multiselect, autocomplete, async and creatable support."
+      />
+      <main
+        id="main"
+        className="w-full flex-1 flex-col items-center justify-center p-5"
+      >
+        <section
+          style={{
+            marginBottom: "20px",
+          }}
+        >
+          <div>
+            <CreatableSelect
+              components={components}
+              inputValue={inputValue}
+              isClearable
+              isMulti
+              menuIsOpen={false}
+              onChange={(newValue) => setValue(newValue)}
+              onInputChange={(newValue) => setInputValue(newValue)}
+              onKeyDown={handleKeyDown}
+              placeholder="Type something and press enter..."
+              value={value}
+            />
+          </div>
+        </section>
+      </main>
+      <Footer>
+        <ol
+          className="pl-6"
+          style={{
+            listStyleType: "number",
+          }}
+        >
+          <li>
+            react-select:
+            <br />
+            <a
+              href="https://react-select.com"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="text-blue-600 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+            >
+              https://react-select.com
+            </a>
+          </li>
+          <li>
+            react-select (GitHub):
+            <br />
+            <a
+              href="https://github.com/jedwatson/react-select"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="text-blue-600 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+            >
+              https://github.com/jedwatson/react-select
+            </a>
+          </li>
+        </ol>
+      </Footer>
+    </div>
+  );
+};
+
+export default ReactSelectPage;
--- src/pages/react-select/index.tsx
+++ src/pages/react-select/index.tsx
@@ -90,6 +90,14 @@ const IndexPage: NextPage = () => {
                 react-select (Creatable: Multi Select)
               </Link>
             </li>
+            <li>
+              <Link
+                href="/react-select/creatable-multi-select-with-text-input"
+                className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+              >
+                react-select (Creatable: Multi Select with Text Input)
+              </Link>
+            </li>
           </ol>
         </article>
       </main>
