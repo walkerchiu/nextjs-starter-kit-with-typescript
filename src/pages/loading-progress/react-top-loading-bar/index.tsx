88fcf2006372a39576e4236f2f3e8cd8c311ed63
walkerchiu <chenjen.chiou@gmail.com>
2023-09-25 15:29:27 +0800

    Feat(Vendor): Add example of react-top-loading-bar

--- src/pages/loading-progress/react-top-loading-bar/index.tsx
+++ src/pages/loading-progress/react-top-loading-bar/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 
-import Footer from "../../layouts/Footer";
-import Header from "../../layouts/Header";
+import Footer from "../../../layouts/Footer";
+import Header from "../../../layouts/Header";
 
 const IndexPage: NextPage = () => {
   return (
     <div className="flex flex-col h-screen">
       <Header
-        title="Loading Progress"
-        description="Explore a collection of diverse progress bar functionalities for seamless webpage transitions."
+        title="react-top-loading-bar"
+        description="A very simple, highly customisable youtube-like react loader component."
       />
       <main
         id="main"
@@ -18,23 +18,39 @@ const IndexPage: NextPage = () => {
         <article>
           <ol
             style={{
+              listStyleType: "disc",
+            }}
+          >
+            <li>
+              <Link
+                href="/loading-progress"
+                className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+              >
+                Loading Progress
+              </Link>
+            </li>
+          </ol>
+        </article>
+        <article className="mt-5">
+          <ol
+            style={{
               listStyleType: "number",
             }}
           >
             <li>
               <Link
-                href="/loading-progress/nprogress"
+                href="/loading-progress/react-top-loading-bar/page?page=1"
                 className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
               >
-                nprogress
+                Page 1
               </Link>
             </li>
             <li>
               <Link
-                href="/loading-progress/react-top-loading-bar"
+                href="/loading-progress/react-top-loading-bar/page?page=2"
                 className="text-blue-700 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
               >
-                react-top-loading-bar
+                Page 2
               </Link>
             </li>
           </ol>
@@ -48,18 +64,6 @@ const IndexPage: NextPage = () => {
           }}
         >
           <li>
-            NProgress.js:
-            <br />
-            <a
-              href="https://rstacruz.github.io/nprogress"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="text-blue-600 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
-            >
-              https://rstacruz.github.io/nprogress
-            </a>
-          </li>
-          <li>
             react-top-loading-bar:
             <br />
             <a
@@ -69,6 +73,18 @@ const IndexPage: NextPage = () => {
               className="text-blue-600 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
             >
               https://klendi.github.io/react-top-loading-bar
+            </a>
+          </li>
+          <li>
+            react-top-loading-bar (Github):
+            <br />
+            <a
+              href="https://github.com/klendi/react-top-loading-bar"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="text-blue-600 dark:text-blue-300 hover:underline hover:text-blue-600 focus:text-blue-600 dark:hover:text-yellow-200 dark:focus:text-yellow-200"
+            >
+              https://github.com/klendi/react-top-loading-bar
             </a>
           </li>
         </ol>
