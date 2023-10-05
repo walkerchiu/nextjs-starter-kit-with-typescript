e4628c61b6091805d2978b51c90bd37f56a0fc09
walkerchiu <chenjen.chiou@gmail.com>
2023-10-05 22:30:16 +0800

    Feat(Module): Replace prompt with react-modal in tiptap-youtube example

--- src/modules/tiptap/components/TextMenuEditorWithYoutube.tsx
+++ src/modules/tiptap/components/TextMenuEditorWithYoutube.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Youtube from "@tiptap/extension-youtube";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Modal from "react-modal";
 
 import styles from "../styles/TextMenuEditorWithYoutube.module.scss";
 
@@ -11,34 +12,104 @@ interface MenuBarProps {
 }
 
 const MenuBar: React.FC<MenuBarProps> = ({ editor }) => {
-  if (!editor) {
-    return null;
-  }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [url, setUrl] = useState("");
+  const [width, setWidth] = useState(640);
+  const [height, setHeight] = useState(480);
 
-  const addYoutubeVideo = () => {
-    const url = prompt("Enter YouTube URL");
-    const widthInput = prompt("Enter Width of the video");
-    const heightInput = prompt("Enter Height of the video");
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleReset = () => {
+    setUrl("");
+    setWidth(640);
+    setHeight(480);
+  };
+
+  const handleSubmit = () => {
     if (url) {
-      const width = widthInput ? Math.max(320, parseInt(widthInput, 10)) : 640;
-      const height = heightInput
-        ? Math.max(180, parseInt(heightInput, 10))
-        : 480;
+      const widthValue = Math.max(320, parseInt(width.toString(), 10));
+      const heightValue = Math.max(180, parseInt(height.toString(), 10));
 
       editor.commands.setYoutubeVideo({
         src: url,
-        width: width,
-        height: height,
+        width: widthValue,
+        height: heightValue,
       });
+
+      closeModal();
     }
   };
 
   return (
     <>
-      <button id="add" onClick={addYoutubeVideo} className={`${styles.button}`}>
+      <button id="add" onClick={openModal} className={`${styles.button}`}>
         Add YouTube video
       </button>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Add YouTube Video Modal"
+        className={styles.Modal}
+      >
+        <h2>Add YouTube Video</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="youtubeUrl">YouTube URL:</label>
+            <input
+              type="text"
+              id="youtubeUrl"
+              value={url}
+              required
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="videoWidth">Width:</label>
+            <input
+              type="number"
+              id="videoWidth"
+              value={width}
+              required
+              onChange={(e) => setWidth(parseInt(e.target.value, 10))}
+            />
+          </div>
+          <div>
+            <label htmlFor="videoHeight">Height:</label>
+            <input
+              type="number"
+              id="videoHeight"
+              value={height}
+              required
+              onChange={(e) => setHeight(parseInt(e.target.value, 10))}
+            />
+          </div>
+          <div className={styles.buttonContainer}>
+            <button
+              type="button"
+              onClick={closeModal}
+              className={styles.closeButton}
+            >
+              Close
+            </button>
+            <button
+              type="reset"
+              onClick={handleReset}
+              className={styles.resetButton}
+            >
+              Reset
+            </button>
+            <button type="submit" className={styles.submitButton}>
+              Add Video
+            </button>
+          </div>
+        </form>
+      </Modal>
     </>
   );
 };
